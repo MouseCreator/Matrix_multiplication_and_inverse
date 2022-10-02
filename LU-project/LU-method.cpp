0f3cbfae64cb62075f6ab6214aa976ab47993687
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2022-10-02 19:47:00 +0300

    Added class Row with operators for easier calculations

--- LU-project/LU-method.cpp
+++ LU-project/LU-method.cpp
@@ -7,7 +7,7 @@
 class Matrix {
 
 private:
-	std::vector<std::vector<double>> table;
+	std::vector<Row> table;
 	int n;
 
 	int extendRow(std::vector<double>& row) {
@@ -21,9 +21,9 @@ private:
 	void addRowFromLine(std::string line) {
 		std::istringstream iss(line);
 		double toAdd;
-		std::vector<double> newRow;
+		Row newRow;
 		while (iss >> toAdd) {
-			newRow.push_back(toAdd);
+			newRow.add(toAdd);
 		}
 		this->table.push_back(newRow);
 		n++;
@@ -33,7 +33,7 @@ private:
 		if (n == 0) {
 			return 0;
 		}
-		std::size_t result = this->table[0].size();
+		std::size_t result = table[0].size();
 		for (int i = 1; i < n; i++) {
 			std::size_t currentSize = this->table[i].size();
 			if (currentSize > result)
@@ -45,63 +45,49 @@ private:
 		std::size_t dimention = maxRowLength();
 		for (int i = 0; i < n; i++) {
 			for (int j = this->table[i].size(); j < dimention; j++) {
-				this->table[i].push_back(0);
+				this->table[i].add(0);
 			}
 		}
 		if (dimention > n) {
-			std::vector<double> emptyRow;
-			for (int i = 0; i < dimention; i++) {
-				emptyRow.push_back(0);
-			}
+			Row emptyRow(dimention);
 			for (; n < dimention; n++) {
 				this->table.push_back(emptyRow);
 			}
 		}
 	}
-	std::vector<double> createRow(int length) {
-		std::vector<double> row;
-		for (int j = 0; j < length; j++) {
-			row.push_back(0);
-		}
-		return row;
-	}
 
 public:
+
+	Matrix operator *(Matrix other) {
+			if (this->n != other.n) {
+				std::cout << "Matrixes has different dimentions!" << std::endl;
+				return Matrix(n);
+			}
+			Matrix result;
+			for (int i = 0; i < n; i++) {
+				Row newRow;
+				result.n++;
+				for (int j = 0; j < n; j++) {
+					double toAdd = 0.0;
+					for (int k = 0; k < n; k++) {
+						toAdd += this->table[i][k] * other.table[k][j];
+					}
+					newRow.add(toAdd);
+				}
+				result.table.push_back(newRow);
+			}
+			return result;
+	}
 	Matrix(int size) {
 		this->n = size;
 		for (int i = 0; i < size; i++) {
-			table.push_back(createRow(size));
+			table.push_back(Row(size));
 		}
 	}
 	Matrix() {
 		n = 0;
 	}
 
-	
-
-	void setSize(int size) {
-		if (n < size) {
-			for (int i = 0; i < n; i++) {
-				for (int j = n; j < size; j++) {
-					table[i].push_back(0);
-				}
-			}
-			for (int i = n; i < size; i++) {
-				table.push_back(createRow(size));
-			}
-		}
-		else {
-			for (int i = 0; i < size; i++) {
-				for (int j = size; j < n; j++) {
-					table[i].pop_back();
-				}
-			}
-			for (int i = size; i < n; i++) {
-				table.pop_back();
-			}
-		}
-		n = size;
-	}
 
 	int getSize() {
 		return this->n;
@@ -148,36 +134,107 @@ public:
 			this->toSquareMatrix();
 		}
 	}
-	Matrix multiplyBy(Matrix &other) {
-		if (this->n != other.n) {
-			std::cout << "Matrixes has different dimentions!" << std::endl;
-			return Matrix(n);
-		}
-		Matrix result;
-		for (int i = 0; i < n; i++) {
-			std::vector<double> newRow;
-			result.n++;
-			for (int j = 0; j < n; j++) {
-				double toAdd = 0.0;
-				for (int k = 0; k < n; k++) {
-					toAdd += this->table[i][k] * other.table[k][j];
-				}
-				newRow.push_back(toAdd);
-			}
-			result.table.push_back(newRow);
-		}
-		return result;
+	
+
+	Matrix inverseLU() {
+		Matrix L;
+		Matrix U;
+
+		std::vector<double> coeficients;
+
+
+
 	}
 
 };
 
+class Row {
+private:
+	std::vector<double> numbers;
+public:
+	Row() {}
+	Row(std::size_t size) {
+		for (int i = 0; i < size; i++) {
+			numbers.push_back(i);
+		}
+	}
+	double operator [](std::size_t i) const {
+		return numbers[i];
+	}
+	double & operator [](std::size_t i) {
+		return numbers[i];
+	}
+	Row operator +(Row& other) {
+		Row result;
+		std::size_t minSize = std::min(this->numbers.size(), other.numbers.size());
 
+		for (std::size_t i = 0; i < minSize; i++) {
+			result.numbers.push_back(this->get(i) + other.get(i));
+		}
+
+		for (std::size_t i = minSize; i < this->numbers.size(); i++) {
+			result.numbers.push_back(this->get(i));
+		}
+
+		for (std::size_t i = minSize; i < other.numbers.size(); i++) {
+			result.numbers.push_back(other.get(i));
+		}
+
+		return result;
+	}
+	Row operator -(Row& other) {
+		Row result;
+		std::size_t minSize = std::min(this->numbers.size(), other.numbers.size());
+
+		for (std::size_t i = 0; i < minSize; i++) {
+			result.numbers.push_back(this->get(i) - other.get(i));
+		}
+
+		for (std::size_t i = minSize; i < this->numbers.size(); i++) {
+			result.numbers.push_back(this->get(i));
+		}
+
+		for (std::size_t i = minSize; i < other.numbers.size(); i++) {
+			result.numbers.push_back(other.get(i));
+		}
+
+		return result;
+	}
+	Row operator *(double& by) {
+		Row result;
+		result.numbers = this->numbers;
+		for (std::size_t i = 0; i < numbers.size(); i++) {
+			result.numbers[i] *= by;
+		}
+		return result;
+	}
+	Row operator /(double& by) {
+		Row result;
+		result.numbers = this->numbers;
+		for (std::size_t i = 0; i < numbers.size(); i++) {
+			result.numbers[i] /= by;
+		}
+		return result;
+	}
+	void add(double value) {
+		numbers.push_back(value);
+	}
+	void set(double value, std::size_t index) {
+		numbers[index] = value;
+	}
+	double get(std::size_t index) {
+		return numbers[index];
+	}
+	std::size_t size() {
+		return numbers.size();
+	}
+};
 
 int main() {
-	Matrix matrix;
+	Matrix matrix1;
 	Matrix matrix2;
-	matrix.loadFromFile("Sample.txt");
+	matrix1.loadFromFile("Sample.txt");
 	matrix2.loadFromFile("Sample2.txt");
-	Matrix result = matrix.multiplyBy(matrix2);
+	Matrix result = matrix1 * matrix2;
 	result.print();
 }
